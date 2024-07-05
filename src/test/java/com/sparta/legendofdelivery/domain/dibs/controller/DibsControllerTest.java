a97458e4a4f3a16a7c1b19f9e6e89a18eb3457d1
Ham-Seung-Wan <syham001@naver.com>
2024-07-05 09:35:44 +0900

    테스트코드

--- src/test/java/com/sparta/legendofdelivery/domain/dibs/controller/DibsControllerTest.java
+++ src/test/java/com/sparta/legendofdelivery/domain/dibs/controller/DibsControllerTest.java
@@ -2,12 +2,13 @@ package com.sparta.legendofdelivery.domain.dibs.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sparta.legendofdelivery.MockSpringSecurityFilter;
+import com.sparta.legendofdelivery.domain.dibs.entity.DibsRank;
+import com.sparta.legendofdelivery.domain.dibs.mapper.DibsRankMapper;
 import com.sparta.legendofdelivery.domain.dibs.service.DibsService;
 import com.sparta.legendofdelivery.domain.user.entity.User;
 import com.sparta.legendofdelivery.domain.user.entity.UserRole;
 import com.sparta.legendofdelivery.domain.user.security.UserDetailsImpl;
 import com.sparta.legendofdelivery.global.config.SecurityConfig;
-import com.sparta.legendofdelivery.global.dto.MessageResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,20 +17,19 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
-import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = DibsController.class, excludeFilters =
@@ -39,38 +39,39 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
                         classes = SecurityConfig.class
                 )
         })
-class DibsControllerTest {
+public class DibsControllerTest {
 
-//    private MockMvc mvc;
-//
-//    private Principal principal;
-//
-//    @Autowired
-//    private WebApplicationContext context;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @MockBean
-//    private DibsService dibsService;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        mvc = MockMvcBuilders.webAppContextSetup(context)
-//                .apply(springSecurity(new MockSpringSecurityFilter()))
-//                .build();
-//        getPrincipal();
-//    }
-//
-//    private void getPrincipal() {
-//
-//        User user = new User();
-//        user.setUserId("testname1234");
-//        user.setRole(UserRole.USER);
-//        UserDetailsImpl userDetails = new UserDetailsImpl(user);
-//        principal = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-//
-//    }
+    private MockMvc mvc;
+
+    private Principal principal;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private DibsService dibsService;
+
+
+    @BeforeEach
+    void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+        getPrincipal();
+    }
+
+    private void getPrincipal() {
+
+        User user = new User();
+        user.setUserId("testname1234");
+        user.setRole(UserRole.USER);
+        UserDetailsImpl userDetails = new UserDetailsImpl(user);
+        principal = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+    }
 //
 //    @Test
 //    @DisplayName("가게 찜 등록 성공 테스트")
@@ -97,4 +98,30 @@ class DibsControllerTest {
     @Test
     void getAllDibsByUser() {
     }
+
+    @Test
+    @DisplayName("찜 많은 TOP10 가게 조회")
+    void getAllDibsByUserTop10() throws Exception {
+        //given
+        List<DibsRankMapper> dibsRank = new ArrayList<>();
+        dibsRank.add(new DibsRank(1, "Store 1"));
+        dibsRank.add(new DibsRank(2, "Store 2"));
+        dibsRank.add(new DibsRank(10, "Store 3"));
+        dibsRank.add(new DibsRank(3, "Store 4"));
+        dibsRank.add(new DibsRank(4, "Store 5"));
+        dibsRank.add(new DibsRank(5, "Store 6"));
+        dibsRank.add(new DibsRank(6, "Store 7"));
+        dibsRank.add(new DibsRank(7, "Store 8"));
+        dibsRank.add(new DibsRank(7, "Store 9"));
+        dibsRank.add(new DibsRank(8, "Store 10"));
+
+        when(dibsService.getDibsRank()).thenReturn(dibsRank);
+        //when
+        //then
+        mvc.perform(get("/api/stores/dibs/rank")
+                        .principal(principal))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+
 }
--- src/test/java/com/sparta/legendofdelivery/domain/dibs/service/DibsServiceTest.java
+++ src/test/java/com/sparta/legendofdelivery/domain/dibs/service/DibsServiceTest.java
@@ -1,14 +1,88 @@
 package com.sparta.legendofdelivery.domain.dibs.service;
 
+import com.querydsl.core.Tuple;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.legendofdelivery.domain.dibs.dto.DibsSearchCond;
+import com.sparta.legendofdelivery.domain.dibs.entity.DibsRank;
+import com.sparta.legendofdelivery.domain.dibs.mapper.DibsRankMapper;
+import com.sparta.legendofdelivery.domain.dibs.repository.DibsRepository;
+import com.sparta.legendofdelivery.domain.store.entity.Category;
+import com.sparta.legendofdelivery.domain.store.entity.Store;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class DibsServiceTest {
+    @Mock
+    DibsRepository dibsRepository;
+
+    @InjectMocks
+    DibsService dibsService;
+
+    @PersistenceContext
+    EntityManager em;
+    JPAQueryFactory jpaQueryFactory;
 
     @BeforeEach
     void setUp() {
+        jpaQueryFactory = new JPAQueryFactory(em);
+    }
+
+    @Test
+    @DisplayName("찜 top 10 가게 조회")
+    void top10() {
+        // given
+        List<DibsRankMapper> mockDibsRank = new ArrayList<>();
+        mockDibsRank.add(new DibsRank(10, "Store 1"));
+        mockDibsRank.add(new DibsRank(9, "Store 2"));
+
+        when(dibsRepository.getDibsRanks()).thenReturn(mockDibsRank);
+
+        // when
+        List<DibsRankMapper> result = dibsService.getDibsRank();
+
+        // then
+        assertNotNull(result);
+        assertEquals(10, result.get(0).getCount());
+        assertEquals("Store 1", result.get(0).getStoreName());
+        assertEquals(9, result.get(1).getCount());
+        assertEquals("Store 2", result.get(1).getStoreName());
+    }
+
+    @Test
+    @DisplayName("가게 검색 필터")
+    void searchCondFilter () {
+        //given
+        Store store = new Store();
+        store.setCategory(Category.KOREAN);
+        store.setDibsCount(15L);
+
+        DibsSearchCond searchCond = DibsSearchCond.builder()
+                .maxDibsCount(20L)
+                .minDibsCount(10L)
+                .category(Category.KOREAN)
+                .build();
+
+        //when
+        List<Tuple> testSearch = dibsService.searchDibs(searchCond,0);
+
+        //then
+        assertNotNull(testSearch);
+        assertEquals(0, testSearch.size());
     }
 
     @Test
